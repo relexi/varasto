6f9d302d28a429e58223aaa9a9adca5c1ebe26af
relexi <alex.roth@gmx.at>
2021-03-17 21:19:17 +0200

    Update db_structures.py

--- test/db_structures.py
+++ test/db_structures.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, DateTime
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
