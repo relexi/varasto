a0f6bc5a97fc015a0a8b52a04772758784100fbc
relexi <alex.roth@gmx.at>
2021-04-12 21:11:16 +0300

    gui entry class Kysely
    began with the class for input form - put it into functions.py, return values and actions not yet implemented

--- test/GUI_select_lava_test.py
+++ test/GUI_select_lava_test.py
@@ -4,7 +4,6 @@ zu Beginn eine Visualisierung der Regale mit Auswahlmöglichkeit der Plätze
 """
 
 import tkinter as tk
-from functions import etsi_paikka, valine_paikalla, uusi_valine
 import functions
 
 
@@ -53,7 +52,7 @@ def displ_lava(pos):
     lbl_info_popup = tk.Label(master=frm_info, text="", bg="white")
     lbl_info_popup.grid(row=0, column=0)
 
-    paikka = etsi_paikka(session, pos)
+    paikka = functions.etsi_paikka(session, pos)
     if paikka.valineet is not []:
         for valine in paikka.valineet:
             print(valine.ta_no)
@@ -67,54 +66,21 @@ def displ_nix(e):
 
 
 def uusi_valine_gui():
-    # this method creates a new valine based on user input.
+    # this function creates a new valine based on user input.
 
-    def valine():
-        val = uusi_valine(
+    """    def valine():
+        val = functions.uusi_valine(
            session, ent_ta_no.get(), ent_luokka.get(), ent_kuvaus.get())
         paikka = functions.etsi_paikka(session, ent_paikka.get())
         paikka.valineet.append(val)
         val.active = 1
         functions.nyt_tapahtuu(session, val, paikka, "sisään",
                                "luotu ja varastoitu")
-        session.commit
+        session.commit()
+    """
 
-    window_in = tk.Tk()
-    window_in.title("uusi väline")
-    window_in.columnconfigure(1, minsize=300)
-
-    lbl_ta_no = tk.Label(window_in, text="VTam-No:")
-    lbl_luokka = tk.Label(window_in, text="luokka:")
-    lbl_kuvaus = tk.Label(window_in, text="kuvaus:")
-    lbl_huom = tk.Label(window_in, text="huomautus:")
-    lbl_paikka = tk.Label(window_in, text="paikka:")
-
-    ent_ta_no = tk.Entry(window_in)
-    ent_luokka = tk.Entry(window_in)
-    ent_kuvaus = tk.Entry(window_in)
-    ent_huom = tk.Entry(window_in)
-    ent_paikka = tk.Entry(window_in)
-
-    btn_tallenna = tk.Button(window_in,
-                             text="Tallenna",
-                             command=valine)
-    btn_peruuta = tk.Button(window_in,
-                            text="Peruuta",
-                            command=window_in.destroy)
-
-    lbl_ta_no.grid(row=0, column=0, sticky="nw")
-    lbl_luokka.grid(row=1, column=0, sticky="nw")
-    lbl_kuvaus.grid(row=2, column=0, sticky="nw")
-    lbl_huom.grid(row=3, column=0, sticky="nw")
-    lbl_paikka.grid(row=4, column=0, sticky="nw")
-    btn_peruuta.grid(row=5, column=0, sticky="nw")
-
-    ent_ta_no.grid(row=0, column=1, sticky="new")
-    ent_luokka.grid(row=1, column=1, sticky="new")
-    ent_kuvaus.grid(row=2, column=1, sticky="new")
-    ent_huom.grid(row=3, column=1, sticky="new")
-    ent_paikka.grid(row=4, column=1, sticky="new")
-    btn_tallenna.grid(row=5, column=1, sticky="new")
+    fields = ["TreVtam", "luokka", "nimi", "huom", "paikka"]
+    frm_valinekysely = functions.Kysely(frm_info, "uusi väline", fields)
 
 
 def nayta_hylly(hylly):
@@ -150,7 +116,7 @@ def nayta_hylly(hylly):
                     highlightthickness=5
                 )
                 frame.grid(row=t, column=lava, padx=8, pady=8)
-                list_valineet = valine_paikalla(session, txt_pos)
+                list_valineet = functions.valine_paikalla(session, txt_pos)
 
                 for valine in list_valineet:
                     str_luokka_id = valine.luokka.luokka_id
@@ -170,15 +136,15 @@ def nayta_hylly(hylly):
 
 window = tk.Tk()
 window.title("varasto")
-window.rowconfigure(0, minsize=700, weight=1)
-window.rowconfigure(1, minsize=200, weight=1)
+window.rowconfigure(0, minsize=800, weight=1)
+window.rowconfigure(1, minsize=300, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
 frm_menu = tk.Frame(window, relief=tk.RAISED, bd=2)
 
 frm_menu.columnconfigure(0, minsize=100, weight=1)
 frm_menu.rowconfigure(0, minsize=500, weight=1)
-frm_menu.rowconfigure(1, minsize=200, weight=1)
+frm_menu.rowconfigure(1, minsize=300, weight=1)
 
 frm_hyllymenu = tk.Frame(frm_menu, relief=tk.RAISED, bd=2)
 frm_alamenu = tk.Frame(frm_menu, relief=tk.RAISED, bd=2)
@@ -187,7 +153,7 @@ frm_hyllymenu.columnconfigure(1, minsize=80, weight=1)
 
 frm_oikea = tk.Frame(window)
 frm_oikea.rowconfigure(0, minsize=500, weight=1)
-frm_oikea.rowconfigure(1, minsize=200, weight=1)
+frm_oikea.rowconfigure(1, minsize=300, weight=1)
 
 frm_info = tk.Frame(frm_oikea, relief=tk.RAISED, bd=2)
 frm_hylly = tk.Frame(frm_oikea, relief=tk.RAISED, bd=2)
--- test/gui_test.py
+++ test/gui_test.py
@@ -1,21 +1,50 @@
-# from tkinter import *
-# import tkMessageBox
 import tkinter as tk
+from tkinter import ttk
+LARGE_FONT = ("Verdana", 12)
 
-top = tk.Tk()
 
-mb = tk.Menubutton ( top, text="condiments", relief=RAISED )
-mb.grid()
-mb.menu =  Menu ( mb, tearoff = 0 )
-mb["menu"] =  mb.menu
+class Kysely:
+    def __init__(self, parent, title, fields):
 
-mayoVar = IntVar()
-ketchVar = IntVar()
+        def full_return(entry_fields):
+            returnfields = {}
+            for field in entry_fields:
+                if str(field["text"]) in (fields):
+                    returnfields.update({field["text"]: field.get()})
+            self.returnfields = returnfields
 
-mb.menu.add_checkbutton ( label="mayo",
-                          variable=mayoVar )
-mb.menu.add_checkbutton ( label="ketchup",
-                          variable=ketchVar )
+        self.title = title
+        self.fields = fields
+        self.parent = parent
+        self.root = tk.Frame(self.parent)
+        self.root.columnconfigure(1, minsize=200)
+        self.root.pack(padx=10, pady=20)
+        self.lbl_title = tk.Label(self.root, text=title, font=LARGE_FONT).grid(
+            row=0, columnspan=2, sticky="ew"
+        )
+        self.entry_fields = []
+        idx = 1
+        for field in fields:
+            lbl = ttk.Label(self.root, text=f"{field} :")
+            ent = ttk.Entry(self.root, text=field)
+            self.entry_fields.append(ent)
+            lbl.grid(row=idx, column=0, sticky="nw", padx=20, pady=5)
+            ent.grid(row=idx, column=1, sticky="nwe", padx=20, pady=5)
+            idx += 1
 
-mb.pack()
-top.mainloop()
+        btn_tallenna = ttk.Button(self.root,
+                                  text="Tallenna",
+                                  command=lambda entry=self.entry_fields:
+                                  full_return(entry))
+        btn_tallenna.grid(row=idx, column=1, sticky="se", padx=20, pady=10)
+        btn_peruuta = ttk.Button(self.root,
+                                 text="Peruuta",
+                                 command=self.root.destroy)
+        btn_peruuta.grid(row=idx, column=0, sticky="sw", padx=20, pady=10)
+
+
+if __name__ == "__main__":
+    fields = ["TreVtam", "luokka", "nimi", "huomautus", "paikka"]
+    window_root = tk.Tk()
+    frame = Kysely(window_root, "uusi väline", fields)
+    window_root.mainloop()
